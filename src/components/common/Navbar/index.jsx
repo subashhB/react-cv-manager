32a39baf2a2ab049f8b5e9cf24991ffc1771dd8d
Admin <subashbaidya9@gmail.com>
2023-07-24 15:47:36 +0545

    feat: Navbar

--- src/components/common/Navbar/index.jsx
+++ src/components/common/Navbar/index.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { AiOutlineBell, AiOutlineUser } from "react-icons/ai";
 
 const Navbar = () => {
-    return <div>index</div>;
+    const username = "User name";
+    return (
+        <div className="m-1 flex  min-w-[960px] rounded-md bg-white p-5 shadow-lg">
+            <div className="ms-auto flex">
+                <div className="relative flex h-12 w-12 cursor-pointer items-center justify-center rounded-3xl bg-white text-gray-900 transition-all duration-300 ease-linear hover:bg-secondary">
+                    <AiOutlineBell size={22} />
+                </div>
+                <div className="m-2 flex items-center font-bold">
+                    Hello, {username}
+                </div>
+                <div className="relative flex h-12 w-12  cursor-pointer items-center justify-center rounded-3xl bg-secondary text-gray-900 transition-all duration-300 ease-linear hover:bg-primary hover:text-white">
+                    <AiOutlineUser size={22} />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/common/Sidebar";
+import Navbar from "../components/common/Navbar";
 
 const RootLayout = () => {
     return (
-        <div>
+        <div className="flex">
             <Sidebar />
-            <Outlet />
+
+            <div className="ms-16">
+                <Navbar />
+                <div className="m-2">
+                    <Outlet />
+                </div>
+            </div>
         </div>
     );
 };
