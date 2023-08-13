722a61609591fd7e20154c61e972a46accd71ec1
Admin <subashbaidya9@gmail.com>
2023-08-13 08:39:19 +0545

    feat: Offer Letter Preview

--- src/components/offerletter/Preview/index.jsx
+++ src/components/offerletter/Preview/index.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import axios from "axios";
+import JoditEditor from "jodit-react";
+import React, { useEffect, useRef, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Preview = () => {
-    return <div>Preview</div>;
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [content, setContent] = useState("");
+    const editorRef = useRef(null);
+    const letterFields = location.state?.letterFields;
+
+    useEffect(() => {
+        if (!letterFields) {
+            navigate("/offerletter");
+        } else {
+            axios
+                .get("http://localhost:3001/LetterTemplates/2")
+                .then((response) => {
+                    const hrDetails = {
+                        hrName: "Riju Shrestha",
+                        hrTitle: "Talent Acquisition and Outreach Specialist",
+                    };
+                    const template = response.data?.Template;
+                    console.log(letterFields);
+                    console.log("Template", template);
+                    const offerLetter = template
+                        .replace("{date}", letterFields.date)
+                        .replace("{applicantName}", letterFields.applicantName)
+                        .replaceAll("{jobTitle}", letterFields.jobTitle)
+                        .replace("{department}", letterFields.department)
+                        .replace(
+                            "{negotiatedSalary}",
+                            letterFields.negotiatedSalary
+                        )
+                        .replace("{HR Name}", hrDetails.hrName)
+                        .replace("{HR Title}", hrDetails.hrTitle);
+                    setContent(offerLetter);
+                });
+        }
+    }, [navigate, letterFields]);
+    if (!letterFields) {
+        return null;
+    }
+
+    const handleSendOfferLetter = () => {
+        console.log("Offer Letter sent");
+    };
+    return (
+        <div>
+            {letterFields && (
+                <>
+                    <Typography variant="h5" sx={{ marginBottom: 3 }}>
+                        Preview
+                    </Typography>
+                    <JoditEditor
+                        ref={editorRef}
+                        value={content}
+                        onChange={(newContent) => setContent(newContent)}
+                    />
+                    <template>{content}</template>
+                    <Box className="flex justify-end w-full my-3">
+                        <Button onClick={() => handleSendOfferLetter()}>
+                            Send FOffer Letter
+                        </Button>
+                    </Box>
+                </>
+            )}
+        </div>
+    );
 };
 
 export default Preview;
